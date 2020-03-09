259967232693465bd772626c6c1c67aaa887083c
xuae <1762799905@qq.com>
2020-03-09 18:09:50 +0800

    引入 ant design 组件

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import './assets/less/ant-theme.less'; // ant design 全局样式
+import './assets/less/global.less'; // 自定义的全局样式
+import './plugins/ant-design'; // ant design 插件
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import Vue from 'vue';
 
 Vue.config.productionTip = false;
+Vue.config.devtools = process.env.NODE_ENV === 'development'; // 生产环境下不允许使用 devtools
 
 new Vue({
   router,
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
+const path = require('path');
+
 module.exports = {
   publicPath: './', // 部署应用包时的基本 URL 为相对路径
   productionSourceMap: false, // 生产环境下的 source map
+
+  css: {
+    extract: true, // 是否使用css分离插件 ExtractTextPlugin
+    sourceMap: false, // 开启 CSS source maps?
+    requireModuleExtension: true, // 启用 CSS modules for all css / pre-processor files.
+
+    // css预设器配置项
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true, // ant design 必须加上
+      },
+    },
+  },
+  pluginOptions: {
+    // 给vue页面加上全局样式和变量，无需在每个vue文件中引入样式（引入多次会导致全局样式类名出现多次）
+    // 依赖包：
+    // style-resources-loader
+    // vue-cli-plugin-style-resources-loader
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        path.resolve(__dirname, 'src/assets/less/ant-vars.less'),
+        path.resolve(__dirname, 'src/assets/less/global.less'),
+      ],
+    },
+  },
 };
