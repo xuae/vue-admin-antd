8f517fca42885aa384229f732c0d1a3cbbcbc172
xuae <xuae.git@foxmail.com>
2020-03-09 16:35:38 +0800

    添加 eslint 配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    publicPath: './', // 部署应用包时的基本 URL 为相对路径
-    productionSourceMap: false, // 生产环境下的 source map
+  publicPath: './', // 部署应用包时的基本 URL 为相对路径
+  productionSourceMap: false, // 生产环境下的 source map
 };
