4e35295f8b78cd7d896f5fb2009aae26f8af3f9f
xuae <55976226+xuae@users.noreply.github.com>
2020-03-11 16:45:17 +0800

    修改文件名的命名格式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,3 @@
-import Home from '../views/Home.vue';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
@@ -7,17 +6,13 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'home',
+    component: () => import('@/views/home.vue'),
   },
   {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    name: 'about',
+    component: () => import('@/views/about.vue'),
   },
 ];
 
