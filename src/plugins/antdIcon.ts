6114ceeb0fde2058f33892f980bd7f30e2bc8cf3
xuae <xuae.git@foxmail.com>
2020-03-11 12:08:21 +0800

    1.添加打包优化 2.添加ant design全局组件按需引入

---
+++ src/plugins/antdIcon.ts
@@ -0,0 +1,45 @@
+export { default as FileOutline } from '@ant-design/icons/lib/outline/FileOutline';
+
+export { default as InboxOutline } from '@ant-design/icons/lib/outline/InboxOutline';
+
+export {
+  default as ShareAltOutline, // share-alt
+} from '@ant-design/icons/lib/outline/ShareAltOutline';
+
+export { default as UserOutline } from '@ant-design/icons/lib/outline/UserOutline';
+
+export { default as LockOutline } from '@ant-design/icons/lib/outline/LockOutline';
+
+// 把antd 的源代码复制过来。
+// var iconType = {
+//     info: 'info-circle',
+//     success: 'check-circle',
+//     error: 'close-circle',
+//     warning: 'exclamation-circle',
+//     loading: 'loading'
+// }[args.type]
+
+// message info
+export { default as InfoCircleOutline } from '@ant-design/icons/lib/outline/InfoCircleOutline';
+export { default as InfoCircleFill } from '@ant-design/icons/lib/fill/InfoCircleFill';
+export { default as InfoCircleTwoTone } from '@ant-design/icons/lib/twotone/InfoCircleTwoTone';
+
+// message success
+export { default as CheckCircleOutline } from '@ant-design/icons/lib/outline/CheckCircleOutline';
+export { default as CheckCircleFill } from '@ant-design/icons/lib/fill/CheckCircleFill';
+export { default as CheckCircleTwoTone } from '@ant-design/icons/lib/twotone/CheckCircleTwoTone';
+
+// message error
+export { default as CloseCircleOutline } from '@ant-design/icons/lib/outline/CloseCircleOutline';
+
+export { default as CloseCircleFill } from '@ant-design/icons/lib/fill/CloseCircleFill';
+
+export { default as CloseCircleTwoTone } from '@ant-design/icons/lib/twotone/CloseCircleTwoTone';
+
+// message warning
+export { default as ExclamationCircleOutline } from '@ant-design/icons/lib/outline/ExclamationCircleOutline';
+
+export { default as ExclamationCircleFill } from '@ant-design/icons/lib/fill/ExclamationCircleFill';
+
+// message loading
+export { default as LoadingOutline } from '@ant-design/icons/lib/outline/LoadingOutline';
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const path = require('path');
+const webpack = require('webpack');
+const isProduction = process.env.NODE_ENV === 'production'; // 是否是生产环境
+
+/**
+ * 代码压缩工具
+ *
+ * @dependency terser-webpack-plugin
+ */
+const TerserPlugin = require('terser-webpack-plugin');
+
+/**
+ * 打包分析工具
+ *
+ * @dependency webpack-bundle-analyzer
+ */
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 /**
  * 若开启了 mock server，需保证项目的端口与此一致
@@ -85,6 +101,7 @@ module.exports = {
       },
     },
   },
+
   pluginOptions: {
     /**
      * 给vue页面加上全局样式和变量，无需在每个vue文件中引入样式（引入多次会导致全局样式类名出现多次）
@@ -99,5 +116,91 @@ module.exports = {
         path.resolve(__dirname, 'src/assets/less/global.less'),
       ],
     },
+  },
+
+  configureWebpack: config => {
+    return {
+      plugins: [
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /zh-cn/), // 只引入 moment 的中文包
+        // new BundleAnalyzerPlugin({ analyzerPort: 8888 }), // js 依赖打包分析
+      ],
+      resolve: {
+        alias: {
+          '@ant-design/icons/lib/dist$': path.resolve(
+            __dirname,
+            './src/plugins/antdIcon.ts'
+          ),
+        },
+      },
+      devtool: !isProduction ? 'cheap-source-map' : false,
+      performance: {
+        // 控制webpack如何通知超出特定文件限制的资产和入口点
+        hints: false, // 关闭提示
+      },
+      optimization: {
+        minimizer: [
+          // ============代码压缩 start============
+          new TerserPlugin({
+            cache: true,
+            parallel: true,
+            sourceMap: false, // Must be set to true if using source-maps in production
+            extractComments: false, // 删除注释
+            terserOptions: {
+              // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+              output: {
+                comments: false, // 删除注释
+              },
+              compress: {
+                warnings: false, // 若打包错误，则注释这行
+                drop_debugger: true,
+                drop_console: true,
+                pure_funcs: ['console.log'],
+              },
+            },
+          }),
+          // ============代码压缩 end============
+        ],
+        splitChunks: {
+          // chunks: 'async', //默认`async`只作用于异步模块，为`all`时对所有模块生效,`initial`对同步模块有效
+          minChunks: 1, // 模块至少被引用的次数
+          // minSize: 30000, // 合并前模块文件的体积，约30Kb
+          // maxSize: 0, // 用于http2和长期缓存
+          maxAsyncRequests: 6, // 异步并行加载的最大代码块数量，默认5
+          maxInitialRequests: 6, // 页面初始化加载的最大代码块数量，默认3
+          // automaticNameDelimiter: '-', //自动命名连接符
+          cacheGroups: {
+            antDesignVue: {
+              name: 'chunk-antd',
+              priority: 20,
+              test: /[\\/]node_modules[\\/]ant-design-vue[\\/]/,
+              chunks: 'all',
+            },
+            libs: {
+              name: 'chunk-libs',
+              priority: 10,
+              test: /[\\/]node_modules[\\/]/,
+              chunks: 'initial', // only package third parties that are initially dependent
+            },
+            commons: {
+              name: 'chunk-commons',
+              test: path.join(__dirname, 'src/components'), // can customize your rules
+              minChunks: 3, //  minimum common number
+              priority: 5,
+              reuseExistingChunk: true,
+            },
+            runtimeChunk: {
+              name: 'runtime',
+            },
+          },
+        },
+      },
+    };
+  },
+
+  chainWebpack(config) {
+    if (isProduction) {
+      config.plugins.delete('preload'); // 移除 preload 插件 TODO: need test
+      config.plugins.delete('prefetch'); // 移除 prefetch 插件 TODO: need test
+    }
   },
 };
