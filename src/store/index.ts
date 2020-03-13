e13234e2be2321ff936f2b632fab4941b7105af6
xuae <1762799905@qq.com>
2020-03-13 16:33:35 +0800

    添加全局变量vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,39 @@
+/**
+ * 状态管理，全局变量
+ *
+ * @dependency vuex
+ */
+
 import Vue from 'vue';
-import Vuex from 'vuex';
+import Vuex, { ModuleTree } from 'vuex';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+/**
+ * 声明 root state
+ */
+export interface StateRoot {}
+
+const modules: ModuleTree<StateRoot> = {};
+
+/**
+ * 将 store 文件夹下的所有ts文件(index.ts除外)，全部导入到modules里面
+ * store 的 module 名就是文件名(不含后缀.ts)
+ */
+const files = require.context('.', false, /.ts$/);
+files.keys().forEach(key => {
+  const fileName = key.replace(/(\.\/|\.ts)/g, '');
+  if (fileName === 'index') {
+    return;
+  }
+  modules[fileName] = files(key);
+});
+
+export default new Vuex.Store<StateRoot>({
   state: {},
-  mutations: {},
+  getters: {},
   actions: {},
-  modules: {},
+  mutations: {},
+  modules: modules,
+  plugins: [],
 });
