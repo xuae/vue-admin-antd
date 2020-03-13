a14adbec9fb6b07389209097b686c4d9fcce72d7
xuae <xuae.git@foxmail.com>
2020-03-13 16:39:40 +0800

    添加布局页面

---
+++ src/components/Layout.vue
@@ -0,0 +1,91 @@
+<!--主面板布局-->
+<template>
+  <a-layout class="layout">
+    <layout-sidebar />
+
+    <a-layout class="layout__container">
+      <layout-header />
+
+      <a-layout-content ref="layoutContent" class="layout__content">
+        <a-back-top class="layout__back" :target="backTopTarget" />
+        <router-view
+          v-if="isRouterAlive"
+          :key="$route.fullPath"
+          class="layout__view"
+        />
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script lang="ts">
+  import { Component, Provide, Ref, Vue } from 'vue-property-decorator';
+  import LayoutHeader from '@/components/LayoutHeader.vue';
+  import LayoutSidebar from '@/components/LayoutSidebar.vue';
+
+  @Component({
+    components: { LayoutHeader, LayoutSidebar },
+  })
+  export default class Layout extends Vue {
+    @Ref() readonly layoutContent!: any;
+
+    isRouterAlive: boolean = true;
+
+    backTopTarget: any = () => window; // 返回顶部按钮的 target
+
+    /**
+     * 刷新子路由当前页面
+     * 用法：
+     * @Inject('reload') readonly reload!: Function; // 需要刷新的页面中注入reload方法
+     * this.reload(); // 调用reload方法，刷新页面
+     */
+    @Provide('reload')
+    reload() {
+      this.isRouterAlive = false;
+      this.$nextTick(function() {
+        this.isRouterAlive = true;
+      });
+    }
+
+    async mounted() {
+      const layoutContent: any = this.$refs.layoutContent;
+      this.backTopTarget = () => layoutContent.$el;
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .layout {
+    display: flex;
+    flex: auto;
+    flex-direction: row;
+    height: calc(100vh - 1px);
+
+    &__container {
+      /*display: block; // 顶部不固定*/
+    }
+
+    &__content {
+      margin: 0;
+      padding: 0 @padding-md;
+      background: transparent;
+      overflow: auto;
+    }
+
+    &__view {
+      margin: @padding-md 0 @padding-lg;
+
+      &.ant-card,
+      /deep/ .ant-card {
+        border: none;
+        margin-bottom: @padding-md;
+      }
+    }
+
+    &__back {
+      box-shadow: @box-shadow;
+      border-radius: 50%;
+      z-index: 30;
+    }
+  }
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,11 @@
 import Vue from 'vue';
-import VueRouter, { RawLocation, Route } from 'vue-router';
+import VueRouter, { RawLocation, Route, RouteConfig } from 'vue-router';
+
+import Layout from '@/components/Layout.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: () => import('@/views/home.vue'),
-  },
+const routes: RouteConfig[] = [
   {
     path: '/about',
     name: 'about',
@@ -18,16 +15,61 @@ const routes = [
     path: '/403',
     name: '403',
     component: () => import('@/views/403.vue'),
+    meta: { hidden: true },
   },
   {
     path: '/404',
     name: '404',
     component: () => import('@/views/404.vue'),
+    meta: { hidden: true },
   },
   {
     path: '/500',
     name: '500',
     component: () => import('@/views/500.vue'),
+    meta: { hidden: true },
+  },
+
+  {
+    path: '/',
+    redirect: '/home',
+    component: Layout,
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        component: () => import('@/views/home.vue'),
+        meta: { title: '首页', icon: 'home' },
+      },
+    ],
+  },
+
+  {
+    path: '/exception',
+    component: Layout,
+    redirect: '/exception/403',
+    name: 'exception',
+    meta: { title: '异常页', icon: 'exclamation-circle' },
+    children: [
+      {
+        path: '403',
+        name: 'exception403',
+        component: () => import('@/views/403.vue'),
+        meta: { title: '403', icon: 'exclamation-circle' },
+      },
+      {
+        path: '404',
+        name: 'exception404',
+        component: () => import('@/views/404.vue'),
+        meta: { title: '404', icon: 'exclamation-circle' },
+      },
+      {
+        path: '500',
+        name: 'exception500',
+        component: () => import('@/views/500.vue'),
+        meta: { title: '500', icon: 'exclamation-circle' },
+      },
+    ],
   },
 ];
 
