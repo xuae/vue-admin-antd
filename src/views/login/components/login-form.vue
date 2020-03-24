1769dc58a2f70493b87ef83abb1f3ca2824f0748
xuae <55976226+xuae@users.noreply.github.com>
2020-03-24 17:17:48 +0800

    添加注册表单，完善登录页面布局

--- src/views/login/components/login-form.vue
+++ src/views/login/components/login-form.vue
@@ -1,101 +1,99 @@
 <template>
-  <div>
-    <a-form class="login__form" :form="form" @submit="handleSubmit">
-      <a-tabs
-        class="login__form__tab"
-        :default-active-key="activeKey"
-        :animated="false"
-        @change="handleTabChange"
-      >
-        <a-tab-pane key="account" tab="账号密码登录">
-          <a-form-item>
-            <a-input
-              v-decorator="[
-                'account',
-                { rules: [{ required: true, message: '请输入账号' }] },
-              ]"
-              size="large"
-              placeholder="账号：admin"
-            >
-              <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
-            </a-input>
-          </a-form-item>
-          <a-form-item>
-            <a-input
-              v-decorator="[
-                'password',
-                { rules: [{ required: true, message: '请输入密码' }] },
-              ]"
-              size="large"
-              type="password"
-              placeholder="密码：123456"
-              autocomplete
-            >
-              <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)" />
-            </a-input>
-          </a-form-item>
-        </a-tab-pane>
-        <a-tab-pane key="mobile" tab="手机号登录" force-render>
-          <a-form-item>
-            <a-input
-              v-decorator="[
-                'mobile',
-                { rules: [{ required: true, message: '请输入手机号' }] },
-              ]"
-              size="large"
-              placeholder="手机号"
-            >
-              <a-icon
-                slot="prefix"
-                type="mobile"
-                style="color:rgba(0,0,0,.25)"
-              />
-            </a-input>
-          </a-form-item>
-          <a-form-item>
-            <a-input-search
-              v-decorator="[
-                'code',
-                { rules: [{ required: true, message: '请输入验证码' }] },
-              ]"
-              size="large"
-              placeholder="验证码"
-            >
-              <a-icon slot="prefix" type="mail" style="color:rgba(0,0,0,.25)" />
-              <a-button slot="enterButton">
-                获取验证码
-              </a-button>
-            </a-input-search>
-          </a-form-item>
-        </a-tab-pane>
-      </a-tabs>
-      <a-form-item>
-        <a-checkbox v-decorator="['autoLogin']">
-          自动登录
-        </a-checkbox>
-        <a href="javascript:;" style="float: right">忘记密码</a>
-      </a-form-item>
-      <a-form-item>
-        <a-button html-type="submit" type="primary" size="large" block>
-          登录
-        </a-button>
-      </a-form-item>
-      <a-form-item class="login__form__other">
-        其他登录方式
-        <a-icon type="qq" />
-        <a-icon type="wechat" />
-        <a-icon type="weibo" />
-        <a href="javascript:;" style="float: right">注册账号</a>
-      </a-form-item>
-    </a-form>
-  </div>
+  <a-form class="login__form" :form="form" @submit="handleSubmit">
+    <a-tabs
+      class="login__form__tab"
+      :default-active-key="activeKey"
+      :animated="false"
+      @change="handleTabChange"
+    >
+      <a-tab-pane key="account" tab="账号密码登录">
+        <a-form-item>
+          <a-input
+            v-decorator="[
+              'account',
+              { rules: [{ required: true, message: '请输入账号' }] },
+            ]"
+            size="large"
+            placeholder="账号：admin"
+          >
+            <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-input
+            v-decorator="[
+              'password',
+              { rules: [{ required: true, message: '请输入密码' }] },
+            ]"
+            size="large"
+            type="password"
+            placeholder="密码：123456"
+            autocomplete
+          >
+            <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+      </a-tab-pane>
+      <a-tab-pane key="mobile" tab="手机号登录" force-render>
+        <a-form-item>
+          <a-input
+            v-decorator="[
+              'mobile',
+              { rules: [{ required: true, message: '请输入手机号' }] },
+            ]"
+            size="large"
+            placeholder="手机号"
+          >
+            <a-icon slot="prefix" type="mobile" style="color:rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-input-search
+            v-decorator="[
+              'code',
+              { rules: [{ required: true, message: '请输入验证码' }] },
+            ]"
+            size="large"
+            placeholder="验证码"
+          >
+            <a-icon slot="prefix" type="mail" style="color:rgba(0,0,0,.25)" />
+            <a-button slot="enterButton">
+              获取验证码
+            </a-button>
+          </a-input-search>
+        </a-form-item>
+      </a-tab-pane>
+    </a-tabs>
+    <a-form-item>
+      <a-checkbox v-decorator="['autoLogin']">
+        自动登录
+      </a-checkbox>
+      <a href="javascript:;" style="float: right">忘记密码</a>
+    </a-form-item>
+    <a-form-item>
+      <a-button html-type="submit" type="primary" size="large" block>
+        登录
+      </a-button>
+    </a-form-item>
+    <a-form-item class="login__form__other">
+      其他登录方式
+      <a-icon type="qq" />
+      <a-icon type="wechat" />
+      <a-icon type="weibo" />
+      <a href="javascript:;" style="float: right" @click="toRegister">
+        注册账号
+      </a>
+    </a-form-item>
+  </a-form>
 </template>
 
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
+  import { Component, Emit, Vue } from 'vue-property-decorator';
 
   @Component
   export default class LoginForm extends Vue {
+    @Emit('to-register') toRegister() {}
+
     activeKey: 'account' | 'mobile' = 'account';
     form: any = null;
 
@@ -129,7 +127,7 @@
     // tab 切换事件
     handleTabChange(activeKey: 'account' | 'mobile') {
       this.activeKey = activeKey;
-      // 清空验证的错误提示
+      // 清空表单的错误提示
       this.form.setFields({
         account: { value: this.form.getFieldValue('account'), errors: null },
         password: { value: this.form.getFieldValue('password'), errors: null },
