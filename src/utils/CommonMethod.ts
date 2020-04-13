338f8d895cc9ef166ab4c55d0df878952b79d385
xuae <1762799905@qq.com>
2020-04-13 18:23:44 +0800

    添加部分公用方法

--- src/utils/CommonMethod.ts
+++ src/utils/CommonMethod.ts
@@ -11,4 +11,40 @@ export default class CommonMethod {
   static isObject(value: any) {
     return Object.prototype.toString.call(value) === '[object Object]';
   }
+
+  /**
+   * 检测是否是数组
+   *
+   * @param value
+   */
+  static isArray(value: any) {
+    // return Object.prototype.toString.call(value) === '[object Array]';
+    return Array.isArray(value);
+  }
+
+  /**
+   * 检测是否是纯数字
+   * NaN 的类型是 number，值是 NaN
+   *
+   * @param value
+   */
+  static isNumber(value: any) {
+    return typeof value === 'number' && !Number.isNaN(value);
+  }
+
+  /**
+   * 检测是否是空值、空数组、空对象
+   *
+   * @param value
+   */
+  static isNull(value: any) {
+    if (value === null || value === undefined || value === '') {
+      return true;
+    } else if (this.isArray(value) && value.length === 0) {
+      return true;
+    } else if (this.isObject(value) && Object.keys(value).length === 0) {
+      return true;
+    }
+    return false;
+  }
 }
